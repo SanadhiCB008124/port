a5626f9e7c14b0bda67fe1a732968d97e750a5e0
Sanadhi2002 <sanadhi2002@gmail.com>
2023-10-30 16:32:33 +0530

    hero

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,12 +1,12 @@
 import styles from "../style";
 import React, { useEffect, useState } from "react";
 
-import bannerImg1 from "../assets/600.jpg";
-import bannerImg2 from "../assets/image.png";
-import bannerImg3 from "../assets/jungkook.jpg";
+import image from "../assets/600.jpg";
+import bannerImg2 from "../assets/lap.png";
+import bannerImg3 from "../assets/image4.png";
 
-const imageArray = [bannerImg1, bannerImg2, bannerImg3];
-const textArray = ["Game Development","Web Development" , "Mobile Development"]
+//const imageArray = [bannerImg1, bannerImg2, bannerImg3];
+const textArray = ["Game Development","Web Development","Mobile App Development"]
 const Hero = () => {
 
     const [count, setCount] = useState(0);
@@ -23,7 +23,7 @@ const Hero = () => {
 
   // `image` is derived state from the image array and current count
   // Take the count mod array length to get the correct computed index
-  const image = imageArray[count % imageArray.length];
+  //const image = imageArray[count % imageArray.length];
   const text = textArray[count % textArray.length];
 
   return (
@@ -31,7 +31,7 @@ const Hero = () => {
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6 bg-gray-gradient `}>
         <div className="flex flex-row justify-between items-center w-full">
        
-          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-gradient text-white ss:leading-[100.8px] leading-[75px] ml-60">
+          <h1 className="flex-1 font-poppins font-semibold text-[62px] text-gradient text-white  ml-60">
             {text}
             <br className="sm:block hidden" />{" "}
           </h1>
@@ -41,7 +41,7 @@ const Hero = () => {
       </div>
 
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={image} alt="billing" className="w-[600px] h-[600px] relative z-[5]" />
+        <img src={image} alt="image" className="w-[600px] h-[600px] relative z-[5]" />
 
      
       </div>
